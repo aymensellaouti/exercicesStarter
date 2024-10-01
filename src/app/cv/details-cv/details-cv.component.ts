0ee1761d6e10b2759bb8a982d3c3bc996f218b8e
aymensellaouti <aymen.sellaouti@gmail.com>
2024-10-01 09:00:17 +0100

    updates

--- src/app/cv/details-cv/details-cv.component.ts
+++ src/app/cv/details-cv/details-cv.component.ts
@@ -9,11 +9,5 @@ import { Cv } from "../model/cv.model";
 export class DetailsCvComponent {
   cv: Cv | null = null;
 
-  constructor() {
-    // const id = this.acr.snapshot.params['id'];
-    // this.cvService.getCvById(id).subscribe({
-    //   next: (cv) => this.cv = cv,
-    //   error: () => this.router.navigate([APP_ROUTES.cv])
-    // })
-  }
+  constructor() {}
 }
